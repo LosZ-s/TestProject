82750e8657cdac47158063b6ec9c01a6e259f070
ASUS <ASUS@10.207.28.59>
2021-01-18 17:05:01 -0600

    Pitufo Fierrudo

--- blinky_freeRTOS_NANO/Src/main.c
+++ blinky_freeRTOS_NANO/Src/main.c
@@ -24,6 +24,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 //Hola perro a ver que
+//Fierro alv fierro
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
