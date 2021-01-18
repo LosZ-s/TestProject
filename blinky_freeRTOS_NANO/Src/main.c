d4706003577d40fa03d8e1acf41964a2a53c20ff
Maskatonian <josemanuelinc1@gmail.com>
2021-01-18 17:03:03 -0600

    Fierro2

--- blinky_freeRTOS_NANO/Src/main.c
+++ blinky_freeRTOS_NANO/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+//Hola perro a ver que
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
